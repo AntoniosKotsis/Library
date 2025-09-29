0f60157b7c06f65c0be42f4f6ca4d36d739434c1
AntoniosKotsis <antoniskotsis7@gmail.com>
2025-09-29 17:33:35 +0300

    Update Book.hpp

--- include/Book.hpp
+++ include/Book.hpp
@@ -1,1 +1,21 @@
+#ifndef BOOK_HPP
+#define BOOK_HPP
+
+#include <string>
+
+using namespace std;
+
+class Book
+{
+        public:
+           string title;
+           string name_writer;
+           int year_edition;
+           string ISBN; // International Standard Book Number
+
+           Book (string,string,int,string);
+           void print_data () const;
+};
+
+#endif
 
